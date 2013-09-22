a4574d634724946b1b293d546443fa83f9b83c0f
Sef Kloninger <sef@kloninger.com>
2013-09-22 00:45:06 -0700

    skip loading logfiles already loaded

--- load_log_mongo.py
+++ load_log_mongo.py
@@ -2,15 +2,13 @@
 
 '''
 Load tracking logs into mongo using pymongo driver.  Correctly
-handles the "event" fields (unjson-ifying it) and creates the
-coures_id
+handles the "event" fields (unjson-ifying it) and creates the course_id
     Created on Nov 8, 2012
     @author: jm, edx.org
 
 usage: ./load_log_mongo.py DB COLL f1 f2
 
-Supports multiple file names, globbed file names, and gzipped 
-files.
+Supports multiple file names, globbed file names, and gzipped files.
 '''
 
 from pymongo import Connection
@@ -18,6 +16,7 @@ import json
 import glob
 import sys
 import gzip
+import datetime
 
 def get_course_id(event):
     """
@@ -39,18 +38,51 @@ def get_course_id(event):
             course_id = "/".join(map(str, a[i+1:i+4]))
     return course_id
 
+def canonical_name(filepath):
+    """
+    Save only the filename and the subdirectory it is in, strip off all prior 
+    paths.  If the file ends in .gz, remove that too.  Convert to lower case.
+    """
+    fname = '/'.join(filepath.lower().split('/')[-2:])
+    if len(fname) > 3 and fname[-3:] == ".gz":
+        fname = fname[:-3]
+    return fname
+
+def insert_imported(imp, filepath, error=0, good=0):
+    """
+    File named filepath was imported, so add it to the "imported" collection
+    """
+    rec = {}
+    rec['_id'] = canonical_name(filepath)
+    rec['date'] = datetime.datetime.utcnow()
+    rec['error'] = error
+    rec['good'] = good
+    try:
+        result = imp.update({'_id': rec['_id']}, rec, upsert=True, safe=True)
+    except DuplicateKeyError:
+        print ("File already imported: %s", fname)
+
+# MAIN
+
+if len(sys.argv) < 4:
+    sys.stderr.write("usage: %s db coll f1 [f2] [f3...]\n\n" % sys.argv[0])
+    sys.stderr.write("For one or more files containing edx tracking logs, insert into the\n")
+    sys.stderr.write("collection given. If files end in .gz they are decompressed on the fly.\n")
+    sys.stderr.write("Files successfully loaded are tracked in collection_incremental, and\n")
+    sys.stderr.write("the file is already there, it is skipped (for incremental loads).\n")
+    sys.exit(1)
+
 db_name = sys.argv[1]
 collection_name = sys.argv[2]
 
-# open connection to mongodb 
+# Get database connection and collections
 connection = Connection('localhost', 27017)
-
 db = connection[db_name]
 events = db[collection_name]
+imp = db[collection_name+"_imported"]
 
-count = 0
-error_count = 0
-nb_lines = 0
+total_error = 0
+total_success = 0
 
 # collect all files from command line
 files = []
@@ -58,15 +90,21 @@ for i in sys.argv[3::]:     # all remaining arguments are lists of files to proc
     for j in glob.glob(i):
         files.append(j)
 
-for f in sorted(files):
-    print "loading ", f
-    if f[-3:].lower() == ".gz":
-        logfile = gzip.open(f)
-    else:
-        logfile = open(f)
+for logfile_path in sorted(files):
+    # if this file has already been imported, skip
+    if imp.find({'_id':canonical_name(logfile_path)}).count():
+        print "skipping", logfile_path
+        continue
 
+    print "loading", logfile_path
+    if logfile_path[-3:].lower() == ".gz":
+        logfile = gzip.open(logfile_path)
+    else:
+        logfile = open(logfile_path)
+
+    this_error = 0
+    this_success = 0
     for event in logfile:
-        nb_lines += 1
         try:
             record = json.loads(event)
             for attribute, value in record.iteritems():
@@ -77,15 +115,22 @@ for f in sorted(files):
             if course_id:
                 record['course_id'] = course_id
             res = events.insert(record)
-            count += 1
-            if count % 500000 == 0:
-                sys.stdout.write("\n")
-            if count % 10000 == 0:
-                sys.stdout.write(".")
         except Exception as e:
-            error_count += 1
+            # TODO: handle different types of exceptions
+            this_error += 1
+        else:
+            this_success += 1
+
+        if (this_error + this_success) % 500000 == 0:
+            sys.stdout.write("\n")
+        elif (this_error + this_success) % 10000 == 0:
+            sys.stdout.write(".")
+
+    insert_imported(imp, logfile_path, error=this_error, good=this_success)
+    total_error += this_error
+    total_success += this_success
     sys.stdout.write("\n")
 
-print "total events read:     %s" % nb_lines
-print "inserted events:       %s" % count
-print "corrupt events:        %s" % error_count
+print "total events read:     %d" % (total_error + total_success)
+print "inserted events:       %d" % total_success
+print "corrupt events:        %d" % total_error
