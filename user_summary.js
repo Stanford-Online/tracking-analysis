f0fe1d341df90a209fd02eb3654594e0fa8ad5f7
Sef Kloninger <sef@kloninger.com>
2013-09-24 18:06:44 -0700

    generalize user summary query: all classes, all users

---
+++ user_summary.js
@@ -0,0 +1,21 @@
+"INFO: summing session stats by course_id and user"
+var course_user_events = db.session.aggregate([
+       {$group: {_id: {course_id: "$_id.course_id",
+                       username: "$value.username"},
+                 num_events: {$sum: "$value.num_events"},
+                 session_sec: {$sum: "$value.session_sec"},
+                 sessions: {$sum: 1} }},
+]);
+
+"INFO: write results in the \"user_summary\" collection"
+db.user_summary.remove();
+db.user_summary.insert(course_user_events.result);
+
+"INFO: users per class (>100 users)"
+var class_users = db.user_summary.aggregate([
+        {$group: {_id: "$_id.course_id",
+                  num_users: {$sum:1} }},
+        {$match: {num_users: {$gt: 100}}},
+        {$sort: {num_users: 1}}
+]);
+class_users.result
